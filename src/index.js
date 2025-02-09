3807c05d2ec44ddff3986a85542ed2cf86a1daeb
Hieudevnekkk <nguyenminhhieu06042004@gmail.com>
2025-02-10 01:03:11 +0700

    connect db, get data

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const app = express();
 const port = 3000;
 
 const route = require('./routes');
+const db = require('./config/db');
+
+//Connect to DB
+db.connect();
 
 // Static file
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,26 +18,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     express.urlencoded({
         extended: true,
-    }),
+    })
 ); // dạng form
 
-                  app.use(express.json());
+app.use(express.json());
 
 // HTTP logger
 // app.use(morgan("combined"));
 
 //Template engine
-                      app.engine(
+app.engine(
     'hbs',
     engine({
         extname: '.hbs',
-    }),
+    })
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http:localhost:${port}`);
+    console.log(`App listening at http:localhost:${port}`);
 });
